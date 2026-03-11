1d21c55e98d69b3e47d6119e1225401e91fcf16e
ValdesCoffee <valdes.2022806@ensdbexcelencia.edu.co>
2026-03-11 09:18:26 -0500

    Agregando variables task 2

--- Funcio.py
+++ Funcio.py
@@ -1,3 +1,13 @@
 #Declaremos las variables para indexar
 import time 
 import sys
+for i in range(1, 11):
+        time.sleep(0.3) # El efecto de "carga"
+        sys.stdout.write("\r****************""")
+        sys.stdout.flush()
+        #Este mensaje busca ser amigable con el usuario,Esta pegado con "\n" Para no hacer tantos prints
+        nombredeproduct= input("\nHola ¡Bienvenido al Inventario!\nIngrese su producto: ")     
+         #Pide el precio como un número real
+        preciodeproduct= float(input("Coloque el precio de su producto: "))      
+        #Pide la cantidad como un numero entero (ya que no existe dame 2.5 de manzanas)
+        cantidadproduct= int(input("Coloca la cantidad de productos: "))   
